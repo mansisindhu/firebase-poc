123c2c00753e1b673bab3ca3d2b3a518b85d37ba
mansisindhu <mansisindhu2909@gmail.com>
2023-01-16 23:58:25 +0530

    Fix add to wishlist action

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleAddToWishlist = () => (docId) => {
+  const handleAddToWishlist = (docId) => () => {
     dispatch(addToWishlist(user?.id, docId));
   };
 
